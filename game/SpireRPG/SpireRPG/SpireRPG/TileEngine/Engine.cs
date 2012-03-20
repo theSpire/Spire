4644d08c3e51ec310a9c2c034d62235397e4f249
Scott <scott.kubo@gmail.com>
2012-03-19 20:29:54 -0700

    Did most of the tileset pieces, still need to work with GamePlayScreen
    Signed-off-by: Scott <scott.kubo@gmail.com>

---
+++ game/SpireRPG/SpireRPG/SpireRPG/TileEngine/Engine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SpireRPG.TileEngine
+{
+    class Engine
+    {
+        #region Field Region
+
+        static int tileWidth;
+        static int tileHeight;
+
+        #endregion
+
+        #region Property Region
+
+        public static int TileWidth
+        {
+            get { return tileWidth; }
+
+        }
+
+        public static int TileHeight
+        {
+            get { return tileHeight; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public static Point VectorToCell(Vector2 position)
+        {
+            return new Point((int)position.X / tileWidth, (int)position.Y / tileHeight);
+        }
+
+        #endregion
+
+    }
+}
